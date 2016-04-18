0a424169b722afe638a3216a93c412f3dc450349
Adam Horvath <adam.a.horvath@ericsson.com>
2016-04-18 15:41:05 +0200

    Added NonCopyable

---
+++ src/NonCopyable.h
@@ -0,0 +1,7 @@
+
+// New way way to prevent assignment and copy-construction
+struct NonCopyable {
+    NonCopyable() = default;
+    NonCopyable(const NonCopyable&) = delete;
+    NonCopyable& operator=(const NonCopyable&) = delete;
+};
